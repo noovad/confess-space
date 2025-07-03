f091505dc6cf34f1a369d78870a43c634745e066
Noovad <novadwynt28@gmail.com>
2025-07-03 16:50:47 +0700

    fix: ensure user cookie is cleared on logout

--- src/app/store/useAuthStore.tsx
+++ src/app/store/useAuthStore.tsx
@@ -101,6 +101,7 @@ export const UseAuthStore = create<AuthState>((set) => ({
     set({ loading: true });
     try {
       await axiosAuth.post("/logout");
+      document.cookie = "user=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
       toast.success("Logout successful!");
       return true;
     } catch (error) {
